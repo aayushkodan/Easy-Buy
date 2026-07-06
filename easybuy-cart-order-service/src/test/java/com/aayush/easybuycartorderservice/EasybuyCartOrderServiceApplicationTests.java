3a74bb99ff0966e598ae1d261cccec72e6702b76
aayush <aayushkodan@gmail.com>
2026-07-06 19:21:11 +0530

    Add API Gateway and Config Server with initial configurations

---
+++ easybuy-cart-order-service/src/test/java/com/aayush/easybuycartorderservice/EasybuyCartOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aayush.easybuycartorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybuyCartOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
